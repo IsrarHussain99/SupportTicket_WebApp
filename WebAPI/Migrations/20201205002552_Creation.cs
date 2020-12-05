247d0aee1767621ff7cabfccffd5578452abb59b
IsrarHussain99 <u1764486@unimail.hud.ac.uk>
2020-12-05 00:56:35 +0000

    Add project files.

---
+++ WebAPI/Migrations/20201205002552_Creation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class Creation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DCandidates",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    issue = table.Column<string>(type: "nvarchar(500)", nullable: true),
+                    date = table.Column<DateTime>(type: "Date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCandidates", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCandidates");
+        }
+    }
+}
---
+++ WebAPI/Models/DCandidate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string name { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string issue { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime date { get; set; }
+
+    }
+}
